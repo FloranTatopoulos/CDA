1d3bf6d2dbb3f1aa5d0ff6f32d3fc31757bf0056
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-04-23 23:28:05 +0200

    initalise mongo database

---
+++ WebApp/API/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "architech-secret-key"
+  };
--- WebApp/API/server.js
+++ WebApp/API/server.js
@@ -12,16 +12,30 @@ app.use((req, res, next) => {
 
 app.use("/static", express.static('./static/'));
 
-// parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cookieSession({
     name: "architech-session",
-    secret: "COOKIE_SECRET", // should use as secret environment variable
+    secret: "COOKIE_SECRET", 
     httpOnly: true
   })
 );
+
+const db = require("./app/models");
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
