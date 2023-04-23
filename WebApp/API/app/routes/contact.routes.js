10dbcfd7e3bab7c9bc2544d843e48442d58bbb7c
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-04-24 01:28:35 +0200

    create route of contact form

---
+++ WebApp/API/app/routes/contact.routes.js
@@ -0,0 +1,12 @@
+const controller = require("../controllers/contact.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  app.post("/api/contact/contact", controller.contact);
+};
