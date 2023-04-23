345d784d7c06d69faed677793ba7d494cfb4849e
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-04-24 00:48:37 +0200

    verify existing email and user

--- WebApp/API/app/middlewares/verifySignUp.js
+++ WebApp/API/app/middlewares/verifySignUp.js
@@ -1,3 +1,34 @@
 const db = require("../models");
 const User = db.user;
 
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+  User.findOne({
+    username: req.body.username
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if (user) {
+      res.status(400).send({ message: "Ce nom d'utilisateur est deja pris!" });
+      return;
+    }
+
+    User.findOne({
+      email: req.body.email
+    }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (user) {
+        res.status(400).send({ message: "Cet email est deja pris!" });
+        return;
+      }
+
+      next();
+    });
+  });
+};
