ed36982b7e58f1a17a13680625cacaf1a244df64
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-04-24 00:50:00 +0200

    update verifysignup

--- WebApp/API/app/routes/auth.routes.js
+++ WebApp/API/app/routes/auth.routes.js
@@ -10,7 +10,12 @@ module.exports = function(app) {
   });
 
   app.post(
-    "/api/auth/signup", controller.signup
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted
+    ],
+    controller.signup
   );
   app.post("/api/auth/signin", controller.signin);
 
