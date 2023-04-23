e63a292249ec017ae2ad603a1bc998900ab54916
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-04-24 01:49:28 +0200

    create logout in auth services

--- WebApp/mon-app/src/services/auth.services.js
+++ WebApp/mon-app/src/services/auth.services.js
@@ -1,3 +1,21 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:3000/api/auth/";
+
+const logout = () => {
+    localStorage.removeItem("user");
+    return axios.post(API_URL + "signout").then((response) => {
+      return response.data;
+    });
+  };
+  
+  const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+
+  const AuthService = {
+    logout,
+    getCurrentUser,
+  }
+  
+  export default AuthService;
