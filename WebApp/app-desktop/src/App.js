99e91e6b90f14cc626345d5ae0f5a8c3d656ffc0
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-06-08 17:45:45 +0200

    create routes

--- WebApp/app-desktop/src/App.js
+++ WebApp/app-desktop/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Blog from "./components/Blog";
 
-function App() {
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+          <Route exact path={"/"} element={<Home />} />
+          <Route exact path={"/home"} element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path={"/blog"} element={<Blog />} />
+        </Routes>
   );
 }
 
