9d95da4a5ed99e5fbd6e5bacb8ef3c813013fe33
Floran Tatopoulos <70632117+FloranTatopoulos@users.noreply.github.com>
2023-06-12 13:52:26 +0200

    create post

--- WebApp/API/server.js
+++ WebApp/API/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express();
+var cors = require('cors');
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
